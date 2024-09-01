46104d3c355970dce8b613c4b7a60dd8dfcea2bf
Tejas P <tejassp08@gmail.com>
2024-08-31 19:53:26 -0700

    Parse flowlog file based on lookup table catergerization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import csv
+from collections import defaultdict
 from AbstractLogParser import AbstractLogParser
 
 class FlowLogParser(AbstractLogParser):
@@ -14,15 +15,35 @@ class FlowLogParser(AbstractLogParser):
 
         return lookup_dict
 
-    def parse(self, flow_log_file, lookup_dict,**kwargs):
-        pass
+    def parse(self, flow_log_file, lookup_dict):
+        tag_count = defaultdict(int)
+        port_protocol_count = defaultdict(int)
+        untagged_count = 0
+
+        with open(flow_log_file, 'r') as file:
+            for line in file:
+                parts = line.split()
+                if len(parts) >= 8 and parts[0] == '2':
+                    dstport = parts[5]
+                    protocol = "tcp" if parts[7] == "6" else "udp"
+                    key = (dstport, protocol)
+                    port_protocol_count[key] += 1
+
+                    if key in lookup_dict:
+                        tag_count[lookup_dict[key]] += 1
+                    else:
+                        untagged_count += 1
+        
+        return tag_count, port_protocol_count, untagged_count
 
     def write(self, tag_count, port_protocol_count, untagged_count,**kwargs):
         pass    
 
     def run(self, lookup_file, flow_log_file):
         lookup_dict = self.read(lookup_file)
+        tag_count, port_protocol_count, untagged_count = self.parse(flow_log_file, lookup_dict)
         # print(lookup_dict)
+        # print(tag_count, port_protocol_count, untagged_count)
 
 
 if __name__ == "__main__":
