2288e70d302cfbef83992bc47ebe6cc58b81ee35
Tejas P <tejassp08@gmail.com>
2024-08-31 18:46:15 -0700

    Abstract Parser class, mock flow-log txt file, csv lookup table created

---
+++ AbstractLogParser.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+class AbstractLogParser(ABC):
+    @abstractmethod
+    def read(self, lookup_file,**kwargs):
+        pass
+
+    @abstractmethod
+    def parse(self, flow_log_file, lookup_dict,**kwargs):
+        pass
+
+    @abstractmethod
+    def write(self, tag_count, port_protocol_count, untagged_count,**kwargs):
+        pass
+
+    @abstractmethod 
+    def run(self, lookup_file, flow_log_file,**kwargs):
+        pass
